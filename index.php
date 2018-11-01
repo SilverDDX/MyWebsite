d0ebdfd0410a6486c946bb55ee7663dbee31a300
SilverDDX <yannis.guillemote@gmail.com>
2018-11-01 14:28:29 +0100

    Second Commit

--- index.php
+++ index.php
@@ -7,18 +7,86 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="style.css">
     <title>Yannis Guillemote</title>
   </head>
   <body>
-    <div class="container">
-      <button class="btn btn-success">Click Me</button>
-    </div>
+    <!--Header-->
+    <div class="container-fluid">
+      <header class="header row">
+            <div class="col-xs-12 col-md-12 col-lg-12">
+              <h1 class="col xs-12 text-center">Yannis Guillemote</h1>
+            </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+          <div class="col-xs-12 col-md-12 col-lg-12">
+              <h2 class="col-xs-12 text-center">Createur de contenus multimedia</h2>
+          </div>
+      </header>
+
+        <div class="social-media row">
+          <section class = "col-10 col-sm-10 col-md-6 col-lg-5 col-xl-4">
+            <div class="row">
+              <div href :"www.google.fr" class="col-4 col-sm-4 col-md-4 col-lg-4">
+                <img src="media/linkedin.svg" class="img-responsive" alt="linkedin">
+              </div>
+
+              <div class="col-4 col-sm-4 col-md-4 col-lg-4">
+                <img src="media/youtube.svg" class="img-responsive" alt="youtube">
+              </div>
+
+              <div class="col-4 col-sm-4 col-md-4 col-lg-4">
+                <img src="media/tumblr.svg" class="img-responsive" alt="tumblr">
+              </div>
+            </div>
+          </section>
+        </div>
+        <div class=" mail row">
+          <div class="col-12 col-sm-12 col-md-12 col-lg-12 text-center">
+                  <h3><small>yannis.guillemote@gmail.com</small></h3>
+          </div>
+
+        </div>
+
+        <div class="row situation">
+            <section class = "col-12 col-sm-12 col-md-12 col-lg-12">
+              <h3 class ="text-center">En recherche de stage <br/>
+                <small>Développement d'applications RA/RV</small>
+              </h3>
+            </section>
+        </div>
+
+        <div class="row resume">
+          <section class="col-12 col-sm-12 col-md-12 col-lg-12">
+              <div class="row">
+                <div class="col-10 col-sm-8 col-md-8 col-lg-5">
+                  <div id="legend"><img src="media/legend.jpg" alt="yannis"></div>
+                  <p class="text-justify">
+                    Originaire de Dijon, je suis étudiant en Master Management des Technologies Interactives 3D aux Arts et Métiers Paritech.
+                  </p>
+                  <p class="text-justify">
+                    Ayant précédemment étudié <strong>l'audiovisuel</strong>, <strong>l'intégration web</strong> et le <strong>graphisme</strong> au cours de mon cursus,
+                    la <strong>découverte</strong> de nouveaux domaines est l'un des moteurs de mes travaux.
+                  </p>
+                  <p class="clear text-justify">
+                    Passionné par l'utilisation de l'outil informatique dans la <strong>création</strong>,
+                    je suis particulièrement intéressé par les projets alliant <strong>art et techologie</strong>.
+                  </p>
+                </div>
+              </div>
+          </section>
+        </div>
+
+
+
+
+        <!-- nav -->
+        <nav class="row nav">
+          <div class="row menu-container">
+            <a class="col-4 nav-link active" href="#">Qui suis-je</a>
+            <a class="col-4 nav-link" href="#">Mon CV</a>
+            <a class="col-4 nav-link" href="#">Portfolio</a>
+          </div>
+        </nav>
+      </div>
   </body>
 </html>
