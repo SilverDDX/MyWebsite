4fbdd170a86bddf64f113f82f0e75b18454dbada
SilverDDX <yannis.guillemote@gmail.com>
2018-11-03 20:20:16 +0100

    Version 1.0

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="media/iconpage.png" />
     <title>Yannis Guillemote</title>
   </head>
   <body>
@@ -19,27 +20,34 @@
             </div>
 
           <div class="col-xs-12 col-md-12 col-lg-12">
-              <h2 class="col-xs-12 text-center">Createur de contenus multimedia</h2>
+              <h2 class="col-xs-12 text-center">Createur de contenus multimedias</h2>
           </div>
       </header>
 
         <div class="social-media row">
           <section class = "col-10 col-sm-10 col-md-6 col-lg-5 col-xl-4">
             <div class="row">
-              <div href :"www.google.fr" class="col-4 col-sm-4 col-md-4 col-lg-4">
-                <img src="media/linkedin.svg" class="img-responsive" alt="linkedin">
-              </div>
+                <div class="col-4 col-sm-4 col-md-4 col-lg-4">
+                  <a href="https://www.linkedin.com/in/yannis-guillemote/">
+                    <img src="media/linkedin.svg" class="img-responsive" alt="linkedin">
+                  </a>
+                </div>
 
-              <div class="col-4 col-sm-4 col-md-4 col-lg-4">
-                <img src="media/youtube.svg" class="img-responsive" alt="youtube">
-              </div>
+                <div class="col-4 col-sm-4 col-md-4 col-lg-4">
+                  <a href="https://www.youtube.com/playlist?list=PLAOsEqUppy7ugLlqJSTEz29faXxytmKuH">
+                    <img src="media/youtube.svg" class="img-responsive" alt="youtube">
+                  </a>
+                </div>
 
-              <div class="col-4 col-sm-4 col-md-4 col-lg-4">
-                <img src="media/tumblr.svg" class="img-responsive" alt="tumblr">
-              </div>
+                <div class="col-4 col-sm-4 col-md-4 col-lg-4">
+                  <a href="https://siliusyg.tumblr.com">
+                    <img src="media/tumblr.svg" class="img-responsive" alt="tumblr">
+                  </a>
+                </div>
             </div>
           </section>
         </div>
+
         <div class=" mail row">
           <div class="col-12 col-sm-12 col-md-12 col-lg-12 text-center">
                   <h3><small>yannis.guillemote@gmail.com</small></h3>
@@ -47,7 +55,7 @@
 
         </div>
 
-        <div class="row situation">
+        <div id ="resume" class="row situation">
             <section class = "col-12 col-sm-12 col-md-12 col-lg-12">
               <h3 class ="text-center">En recherche de stage <br/>
                 <small>Développement d'applications RA/RV</small>
@@ -57,10 +65,12 @@
 
         <div class="row resume">
           <section class="col-12 col-sm-12 col-md-12 col-lg-12">
+                <h1 class="text-center">Qui suis-je ?</h1>
+
               <div class="row">
-                <div class="col-10 col-sm-8 col-md-8 col-lg-5">
+                <div class="col-10 col-sm-8 col-md-8 col-lg-6">
                   <div id="legend"><img src="media/legend.jpg" alt="yannis"></div>
-                  <p class="text-justify">
+                  <p class="first-paragraph text-justify">
                     Originaire de Dijon, je suis étudiant en Master Management des Technologies Interactives 3D aux Arts et Métiers Paritech.
                   </p>
                   <p class="text-justify">
@@ -69,24 +79,145 @@
                   </p>
                   <p class="clear text-justify">
                     Passionné par l'utilisation de l'outil informatique dans la <strong>création</strong>,
-                    je suis particulièrement intéressé par les projets alliant <strong>art et techologie</strong>.
-                  </p>
+                    je suis particulièrement intéressé par les projets alliant <strong>art et technologie</strong>.
                 </div>
               </div>
+                <a class="wide-clear info-button" href="media/CV2018.pdf">
+                <div class="text-center cv-link col-5 col-sm-4 col-md-3 col-lg-2">
+                  Pour plus d'informations, voir mon CV
+                </div>
+                </a>
+
+
           </section>
         </div>
 
+        <!-- PT1 -->
+        <div id="portfolio" class="row portfolio pt1">
+          <section class="col-11 col-sm-11 col-md-10 col-lg-8">
+                  <h1 class="text-center">Portfolio</h1>
+                  <h2 class="text-center">Modélisation 3D</h2>
+
+                <div class="row">
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img data="media/portfolio/final.png" src="media/portfolio/final-thumb.png" alt="ile-volante">
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img data="media/portfolio/SHAKER.jpg" src="media/portfolio/SHAKER-thumb.jpg" alt="shaker">
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img data="media/portfolio/building.jpg" src="media/portfolio/building-thumb.jpg" alt="building">
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img data="media/portfolio/noodle.png" src="media/portfolio/noodle-thumb.png" alt="vaisseau">
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img data="media/portfolio/weird.png" src="media/portfolio/weird-thumb.png" alt="weird1">
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img data="media/portfolio/weird2.png" src="media/portfolio/weird2-thumb.png" alt="weird2">
+                </div>
+            </div>
+          </section>
+        </div>
+
+        <!-- PT2 -->
+        <div class="row portfolio pt2">
+          <section class="col-11 col-sm-11 col-md-10 col-lg-8">
+                  <h2 class="text-center">Production audiovisuelle</h2>
+
+                <div class="row">
+
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <iframe width="100%" height="90%" src="https://www.youtube.com/embed/eX5rPoqHPik" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+              <iframe width="100%" height="90%" src="https://www.youtube.com/embed/VCl4a-m8XNs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+              </iframe>
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <iframe width="100%" height="90%" src="https://www.youtube.com/embed/lArToGPnH2c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+          </section>
+        </div>
+
+        <!-- PT3 -->
+        <div class="row portfolio pt3">
+          <section class="col-11 col-sm-11 col-md-10 col-lg-8">
+                  <h2 class="text-center">Photographie</h2>
+                <div class="row">
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img data="media/portfolio/phot1.jpg" src="media/portfolio/phot1-thumb.jpg" alt="clavier">
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img data="media/portfolio/phot2.jpg" src="media/portfolio/phot2-thumb.jpg" alt="guitares">
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img data="media/portfolio/phot3.jpg" src="media/portfolio/phot3-thumb.jpg" alt="livre">
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img data="media/portfolio/phot4.jpg" src="media/portfolio/phot4-thumb.jpg" alt="chenille">
+                </div>
+
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img data="media/portfolio/phot5.jpg" src="media/portfolio/phot5-thumb.jpg" alt="café">
+                </div>
+            </div>
+          </section>
+        </div>
+
+        <div class="row portfolio pt4">
+          <section class="col-11 col-sm-11 col-md-10 col-lg-8">
+                  <h2 class="text-center">Graphisme / Infographie</h2>
+                <div class="row">
+
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img class="scroll" data="media/portfolio/GR3.jpg" src="media/portfolio/GR3-thumb.jpg" alt="livre">
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img class="scroll" data="media/portfolio/GR5.jpg" src="media/portfolio/GR5-thumb.jpg" alt="café">
+                </div>
+
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img class="scroll" data="media/portfolio/GR2.jpg" src="media/portfolio/GR2-thumb.jpg" alt="guitares">
+                </div>
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img class="scroll" data="media/portfolio/GR6.jpg" src="media/portfolio/GR6-thumb.jpg" alt="café">
+                </div>
+
+                <div class="ptf-item col-12 col-sm-12 col-md-6 col-lg-4">
+                  <img class="scroll" data="media/portfolio/WB3.jpg" src="media/portfolio/WB3-thumb.jpg" alt="web">
+                </div>
 
 
+            </div>
+          </section>
+    </div>
 
+    <div class="row footer">
+      <footer class="col-10 col-sm-10 col-md-8 col-xl-6">
+        <p class="text-center">Site et contenus réalisés par Yannis Guillemote ou associés consentants, tous droits réservés</p>
+      </footer>
+    </div>
         <!-- nav -->
         <nav class="row nav">
           <div class="row menu-container">
-            <a class="col-4 nav-link active" href="#">Qui suis-je</a>
-            <a class="col-4 nav-link" href="#">Mon CV</a>
-            <a class="col-4 nav-link" href="#">Portfolio</a>
+            <a class="col-4 nav-link" href="#resume">Qui suis-je</a>
+            <a class="col-4 nav-link" href="media/CV2018.pdf">Mon CV</a>
+            <a class="col-4 nav-link" href="#portfolio">Portfolio</a>
           </div>
         </nav>
+        <div id="popup">
+
+        <span id="exit"><img src="media/fleche.png" alt="fleche"></span>
+        <div><img src="" alt="zoom"/></div>
+
+      </div>
+
       </div>
   </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="main.js"></script>
+
 </html>
